ce4eeff475880cb490bef4f67c01a73d7025ba4c
Sachin-094 <sachinawsdemo94@gmail.com>
2024-08-30 21:12:43 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,9 @@
 # app.py
 # This is a test commit
-# This is mu second commit
+# This is my second commit
 def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert add(1, 2) == 4
+    assert add(1, -1) == 2
